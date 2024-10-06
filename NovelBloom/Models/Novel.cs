d516eb111744ac66f7fccf1d2b439ffe2a42ea72
Ragnus531 <jakub8327@gmail.com>
2024-10-06 17:52:05 +0200

    layout created

---
+++ NovelBloom/Models/Novel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovelBloom.Models;
+
+internal class Novel
+{
+    public string Title { get; set; }
+    public AppUser User { get; set; }
+    public string ShortDescription { get; set; }
+    public NovelDetail NovelDetail { get; set; }
+}
